45b985125b154fe662ca26943b648c214a2bb2f2
chloyka <chloyka@gmail.com>
2023-08-27 16:23:22 -0300

    chore: add export gopath and goroot to pre-commit script

--- tools/optimize.sh
+++ tools/optimize.sh
@@ -6,6 +6,10 @@ if ! command -v go version &> /dev/null ; then
     echo "go not installed or available in the PATH" >&2
     exit 1
 fi
+
+export PATH="$(go env GOPATH)/bin:$PATH"
+export PATH="$(go env GOROOT)/bin:$PATH"
+
 # if file bin/optimizer not exists, build it
 if ! command -v image-optimizer &> /dev/null ; then
   go install github.com/chloyka/chloyka.com/tools/image-optimizer@latest
